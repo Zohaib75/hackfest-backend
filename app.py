979df89ca9bb96fb5630468a33ca28f67062db87
Zohaib Sohail <i.zohaib@outlook.com>
2021-09-05 17:18:14 +0500

    comments added in request form

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            comments = request.form.get('comments')
             return 'File successfully uploaded' # add result string here
 
         abort(400, 'File Type not allowed.') 
